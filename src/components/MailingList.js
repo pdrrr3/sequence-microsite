2a3881bfb0d38f816d98e7c95eb3aee5af31cb84
Daniel Whiffing <danielwhiffing@gmail.com>
2020-08-26 14:07:19 -0400

    Basic mailing list

--- src/components/MailingList.js
+++ src/components/MailingList.js
@@ -1,11 +1,11 @@
 import React from "react"
 
 export const MailingList = () => (
-  <div className="mx-auto max-w-2xl my-5">
-    <h3>Join our mailing list for updates and exclusive news</h3>
+  <div className="mx-auto max-w-2xl mb-64">
     <form>
-      <div className="bg-offwhite">
-        <input />
+      <div className="flex flex-col items-center">
+        <h3>Join our mailing list for updates and exclusive news</h3>
+        <input className="bg-offwhite self-stretch px-6 py-4 rounded-full mt-4" />
       </div>
     </form>
   </div>
