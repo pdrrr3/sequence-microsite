ed9ca2e72c51fa3b9147368752a5c6ace403c7e3
Daniel Whiffing <danielwhiffing@gmail.com>
2020-09-02 20:26:07 -0400

    Adds 404

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Head } from "../components/Head"
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+import "../index.css"
+
+export default function Home() {
+  return (
+    <div style={{ overflowX: "hidden" }}>
+      <Head />
+      <Header />
+      <div className="container text-center my-64">
+        <h2>404</h2>
+        <p>Not found</p>
+      </div>
+      <Footer />
+    </div>
+  )
+}
