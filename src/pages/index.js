a7b309e2aa8deea5a45e46c09944174217a832c5
Daniel Whiffing <danielwhiffing@gmail.com>
2020-12-09 11:58:44 -0500

    Adds code

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,14 @@ import { Footer } from "../components/Footer"
 import "sal.js/dist/sal.css"
 import "../styles/index.js"
 
-export default function Home() {
+export default function Home({ location }) {
+  const code = new URLSearchParams(location.search).get("code")
+
   useEffect(() => {
     sal()
   }, [])
+
+  if (code !== "iZJDEdNzcQ84MNkmVV") return null
 
   return (
     <div style={{ overflowX: "hidden" }}>
