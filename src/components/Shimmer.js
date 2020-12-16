e829c848c278b552e7375a02be92634a33ed89c9
Daniel Whiffing <danielwhiffing@gmail.com>
2020-12-16 11:16:57 -0500

    shimmer animation

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,30 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import shimmer from "../assets/shimmer.mp4"
 
-export const Shimmer = ({ offset = "-50%", height = "110vw" }) => (
-  <div
-    style={{
-      position: "absolute",
-      overflow: "hidden",
-      width: "100vw",
-      height,
-      opacity: 0.7,
-      zIndex: -2,
-      transform: `translateY(${offset})`,
-    }}
-  >
-    <video
-      autoPlay
-      muted
-      loop
-      className="flex-1"
+export const Shimmer = ({ offset = "-50%", height = "110vw" }) => {
+  const [loaded, setLoaded] = useState(false)
+  return (
+    <div
       style={{
         position: "absolute",
-        width: "100%",
-        transform: `translateY(-20%)`,
+        overflow: "hidden",
+        width: "100vw",
+        height,
+        opacity: 0.7,
+        zIndex: -2,
+        transform: `translateY(${offset})`,
       }}
     >
-      <source src={shimmer} />
-    </video>
-  </div>
-)
+      <video
+        autoPlay
+        muted
+        loop
+        onLoadedData={() => setLoaded(true)}
+        className="flex-1"
+        style={{
+          position: "absolute",
+          width: "100%",
+          opacity: loaded ? 1 : 0,
+          transition: "opacity 2s",
+          transform: `translateY(-20%)`,
+        }}
+      >
+        <source src={shimmer} />
+      </video>
+    </div>
+  )
+}
